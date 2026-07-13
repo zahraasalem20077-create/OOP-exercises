d34c43697ac4de1522fc6a2b8933e498c150526e
zahraa <zahraa.salem.20077@gmail.com>
2026-07-13 17:13:11 +0300

    edit

--- CHAPTER11/EX2.cpp
+++ CHAPTER11/EX2.cpp
@@ -3,14 +3,15 @@
 #include "tool1.h"
 #include "tool2.h"
 using namespace std;
-int main(){
-double num1,num2;
-cout<<"Enter the first number\n";
-cin>>num1;
-cout<<"Enter the secound number\n";
-cin>>num2;
-cout<<left<<setw(10)<<"Sum = "<<TOOL1::calculate(num1,num2)<<endl;
-cout<<left<<setw(10)<<"Product = "<<TOOL2::calculate(num1,num2)<<endl;
+int main()
+{
+    double num1,num2;
+    cout<<"Enter the first number\n";
+    cin>>num1;
+    cout<<"Enter the secound number\n";
+    cin>>num2;
+    cout<<left<<setw(10)<<"Sum = "<<TOOL1::calculate(num1,num2)<<endl;
+    cout<<left<<setw(10)<<"Product = "<<TOOL2::calculate(num1,num2)<<endl;
 
-return 0;
+    return 0;
 }
